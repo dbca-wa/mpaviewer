59e48f9bd7311234ac0db2895f454215213c60ca
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2022-08-24 14:38:58 +0800

    Add download_data, WIP
    * Recursive download returns nothing (bug?)
    * Download falls over folders

---
+++ R/download_data.R
@@ -0,0 +1,37 @@
+#' Donwload all files from a Google Drive folder to a local folder
+#'
+#' NOTE this function does not work yet
+#'
+#' @param drive_folder The URL of the Google Drive folder,
+#'   default: `Sys.getenv("GD_MPAVIEWER")`
+#' @param api_key A read-permitted Google API key,
+#'   default: `Sys.getenv("GOOGLE_API_KEY")`.
+#'   Note the Google Drive API must be enabled.
+#' @param data_dir A local directory to download files to,
+#'   default: `here::here("inst/data")`.
+#'
+#' @return None.
+#' @import googledrive
+#' @export
+download_data <- function(drive_folder = Sys.getenv("GD_MPAVIEWER"),
+                          api_key = Sys.getenv("GOOGLE_API_KEY"),
+                          data_dir = here::here("inst/data")) {
+  # Gatechecks
+  if (missing(drive_folder)){ stop("Missing drive_folder") }
+  if (missing(api_key)){ stop("Missing api_key") }
+
+  # Authentication
+  drive_auth_configure(api_key = api_key)
+
+  # List files
+  files <- drive_ls(path=drive_folder, recursive = TRUE)
+
+  # Gatecheck
+  if (nrow(files) == 0) {abort("No files were found on Google Drive.")}
+
+  # Download files
+  for (file_id in files$id) {
+    drive_download(as_id(file_id), overwrite=TRUE)
+  }
+
+}
