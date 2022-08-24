bed68f599fd5222c13a64b7fd7ec6bc16789efc3
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2022-08-24 15:58:28 +0800

    Download the final data rds from CKAN

--- R/download_data.R
+++ R/download_data.R
@@ -1,12 +1,9 @@
-#' Download all files from a Google Drive folder to a local folder
+#' Download the data file from CKAN
 #'
-#' NOTE this function does not work yet
-#'
-#' @param drive_folder The URL of the Google Drive folder,
-#'   default: `Sys.getenv("GD_MPAVIEWER")`
-#' @param api_key A read-permitted Google API key,
-#'   default: `Sys.getenv("GOOGLE_API_KEY")`.
-#'   Note the Google Drive API must be enabled.
+#' @param ckan_url The data catalogue URL,
+#'   default: `Sys.getenv("CKAN_URL")`
+#' @param api_key The resource ID of the data file,
+#'   default: `Sys.getenv("MPAVIEWER_RID")`.
 #' @param data_dir A local directory to download files to,
 #'   default: `here::here("inst/data")`.
 #'
@@ -14,19 +11,14 @@
 #' @import googledrive
 #' @export
 download_data <- function(ckan_url = Sys.getenv("CKAN_URL"),
-                          ckan_key = Sys.getenv("CKAN_API_KEY"),
                           data_rid = Sys.getenv("MPAVIEWER_RID"),
                           data_dir = here::here("inst/data")) {
 
-  # ckanr::ckanr_setup(url=ckan_url, key=ckan_key)
-  # res <- ckanr::resource_show(data_rid)
-  # curl::curl_download(res$url, here::here("inst/data/mpa_data.rds"))
-  # httr::GET(res$url, httr::write_disk(here::here("inst/data/mpa_data.rds"), overwrite=TRUE))
+  ckanr::ckanr_setup(url=ckan_url)
+  res_url <- ckanr::resource_show(data_rid)$url
+  dest_fn <- here::here(data_dir, "mpa_data.rds")
+  httr::GET(res_url, httr::write_disk(dest_fn, overwrite=TRUE))
 
-  url <- "https://data.dbca.wa.gov.au/dataset/e3e8e90d-f9b5-4e6f-9607-3e50d101f689/resource/29183116-2409-4213-90c7-69f00888f280/download/mpa_data.rds"
-  download.file(url, here::here("inst/data/mpa_data.rds"))
-
-  # This does not want to work today.
   # # Gatechecks
   # if (missing(drive_folder)) {
   #   stop("Missing drive_folder")
