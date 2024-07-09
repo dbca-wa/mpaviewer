493fe7767cbd17377d7b73689bd33482b9fe2668
Brooke Gibbons <00097191@uwa.edu.au>
2024-07-09 16:16:15 +0800

    turn off for deployment

--- app.R
+++ app.R
@@ -1,8 +1,8 @@
-# Authenticate GoogleDrive and GoogleSheets ----
-options(gargle_oauth_cache = ".secrets")
-# check the value of the option, if you like
-gargle::gargle_oauth_cache()
-googlesheets4::gs4_auth()
+## Authenticate GoogleDrive and GoogleSheets ----
+# options(gargle_oauth_cache = ".secrets")
+## check the value of the option, if you like
+# gargle::gargle_oauth_cache()
+# googlesheets4::gs4_auth()
 
 ## If CheckEM package has changed you will need to remove the package and reinstall ----
 # remove.packages("CheckEM")
@@ -14,10 +14,10 @@ googlesheets4::gs4_auth()
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 
 # Step 2. Download all data from GoogleDrive (only run this section if it has been updated) ----
-mpaviewer::googledrive_download_data(raw_dir = "G:/mpaviewer_data", project_dir = "G:/mpaviewer") # takes ~15 minutes to run
+# mpaviewer::googledrive_download_data(raw_dir = "G:/mpaviewer_data", project_dir = "G:/mpaviewer") # takes ~15 minutes to run
 
 # Step 3. Generate new summarised data ----
-mpaviewer::generate_data(raw_dir = "G:/mpaviewer_data") # takes ~5 minutes to run
+# mpaviewer::generate_data(raw_dir = "G:/mpaviewer_data") # takes ~5 minutes to run
 
 # Step 4. Generate new plots
 # Sys.time() # Takes a few hours to run
